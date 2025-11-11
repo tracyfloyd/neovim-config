6b231f85a3909ed33c0e2632dbc2559bbdec36ea
Tracy Floyd <tracyfloyd@gmail.com>
2025-11-10 21:16:34 -0500

    Add statuscol plugin

---
+++ lua/plugins/statuscol.lua
@@ -0,0 +1,26 @@
+-- statuscol
+-- Control the left status column
+-- @link https://github.com/luukvbaal/statuscol.nvim
+
+return {
+  {
+    'luukvbaal/statuscol.nvim',
+    opts = function()
+      local builtin = require('statuscol.builtin')
+      return {
+        setopt = true,
+        -- override the default list of segments with:
+        -- number-less fold indicator, then signs, then line number & separator
+        segments = {
+          { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+          { text = { '%s' }, click = 'v:lua.ScSa' },
+          {
+            text = { builtin.lnumfunc, ' ' },
+            condition = { true, builtin.not_empty },
+            click = 'v:lua.ScLa',
+          },
+        },
+      }
+    end,
+  },
+}
