ef449f891243cb8219265e9060b41a19d81a9a83
Tracy Floyd <tfloyd@reasononeinc.com>
2025-11-20 09:13:58 -0500

    Foramtting

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,20 +1,16 @@
-
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Go to file listing" }) -- Disabled due to Oil.nvim
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
+vim.keymap.set('n', '<leader>cf', function()
+  require('conform').format({
+    lsp_format = 'fallback',
+  })
+end, { desc = 'Conform - Format current file' })
 
-vim.keymap.set("n", "<leader>cf", function()
-    require("conform").format({
-        lsp_format = "fallback",
-    })
-end, { desc = "Conform - Format current file" })
+vim.keymap.set('n', '-', '<cmd>Oil --float<CR>', { desc = "Oil - Open current file's parent directory" })
 
-
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Oil - Open current file's parent directory" })
-
-
-vim.keymap.set("n", "gl", function()
-    vim.diagnostic.open_float()
-end, { desc = "Open Diagnostics in Float" })
+vim.keymap.set('n', 'gl', function()
+  vim.diagnostic.open_float()
+end, { desc = 'Open Diagnostics in Float' })
