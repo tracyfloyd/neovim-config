48c27ad252d515b351686039682e70893f3be9a7
Tracy Floyd <tracyfloyd@gmail.com>
2025-12-11 06:33:54 -0500

    Change color highlighter plugin

---
+++ lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,12 @@
+-- nvim-highlight-colors
+--
+-- Highlight colors
+--
+-- @link https://github.com/brenoprata10/nvim-highlight-colors
+
+return {
+  'brenoprata10/nvim-highlight-colors',
+  config = function()
+    require('nvim-highlight-colors').setup({})
+  end,
+}
