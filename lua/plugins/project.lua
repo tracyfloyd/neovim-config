e40b30c95378d82ec8e1137dd63d8b0d89199b57
Tracy Floyd <tfloyd@reasononeinc.com>
2025-12-18 16:26:15 -0500

    Disable proejct plugin to prevent cwd shenanigans

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,12 +1,12 @@
 -- @link https://github.com/ahmedkhalf/project.nvim
 
 return {
-  'ahmedkhalf/project.nvim',
-  init = function()
-    require('project_nvim').setup({
-      detection_methods = { 'lsp', 'pattern' },
-      patterns = { 'wp-config.php', '*.sln', '.git' },
-    })
-    require('telescope').load_extension('projects')
-  end,
+  -- 'ahmedkhalf/project.nvim',
+  -- init = function()
+  --   require('project_nvim').setup({
+  --     detection_methods = { 'lsp', 'pattern' },
+  --     patterns = { 'wp-config.php', '*.sln', '.git' },
+  --   })
+  --   require('telescope').load_extension('projects')
+  -- end,
 }
