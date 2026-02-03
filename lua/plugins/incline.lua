e4325f73738f777039b9ef7937a84ec8f609ddaf
Tracy Floyd <tfloyd@reasononeinc.com>
2026-02-03 07:32:57 -0500

    Add incline plugin

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,15 @@
+-- incline
+--
+-- Lightweight floating statuslines
+--
+-- @link https://github.com/b0o/incline.nvim
+
+return {
+  'b0o/incline.nvim',
+  -- enabled = false,
+  config = function()
+    require('incline').setup()
+  end,
+  -- Optional: Lazy load Incline
+  event = 'VeryLazy',
+}
