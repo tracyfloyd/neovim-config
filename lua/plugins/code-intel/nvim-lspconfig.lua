6da4bba4b27c12d7bde0987e9cff36c70d00abea
Tracy Floyd <tracyfloyd@gmail.com>
2026-02-09 00:10:04 -0500

    Fix vim globals warnings without lazydev plugin

--- lua/plugins/code-intel/nvim-lspconfig.lua
+++ lua/plugins/code-intel/nvim-lspconfig.lua
@@ -9,17 +9,6 @@ return {
 
   dependencies = {
     {
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-        },
-      },
-    },
-    {
       'j-hui/fidget.nvim',
     },
     {
