baf6226bd40747ca1c41ff4670a76637102a8200
Tracy Floyd <tfloyd@reasononeinc.com>
2026-02-18 12:44:55 -0500

    PLugin updates

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,8 +1,7 @@
 -- opencode
+-- @link https://github.com/NickvanDyke/opencode.nvim
 --
 -- Integrate the opencode AI assistant with Neovim — streamline editor-aware research, reviews, and requests.
---
--- @link https://github.com/NickvanDyke/opencode.nvim
 
 return {
   'NickvanDyke/opencode.nvim',
@@ -25,9 +24,11 @@ return {
     vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
       require('opencode').ask('@this: ', { submit = true })
     end, { desc = 'Ask opencode' })
+
     vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
       require('opencode').select()
     end, { desc = 'Execute opencode action…' })
+
     vim.keymap.set({ 'n', 't' }, '<C-.>', function()
       require('opencode').toggle()
     end, { desc = 'Toggle opencode' })
@@ -35,6 +36,7 @@ return {
     vim.keymap.set({ 'n', 'x' }, 'go', function()
       return require('opencode').operator('@this ')
     end, { expr = true, desc = 'Add range to opencode' })
+
     vim.keymap.set('n', 'goo', function()
       return require('opencode').operator('@this ') .. '_'
     end, { expr = true, desc = 'Add line to opencode' })
@@ -42,6 +44,7 @@ return {
     vim.keymap.set('n', '<S-C-u>', function()
       require('opencode').command('session.half.page.up')
     end, { desc = 'opencode half page up' })
+
     vim.keymap.set('n', '<S-C-d>', function()
       require('opencode').command('session.half.page.down')
     end, { desc = 'opencode half page down' })
