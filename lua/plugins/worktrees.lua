cce31020ea3b0fccf924e69b02545cb37a32e619
Tracy Floyd <tracyfloyd@gmail.com>
2026-04-04 13:02:28 -0400

    Adjust default worktrees path

--- lua/plugins/worktrees.lua
+++ lua/plugins/worktrees.lua
@@ -8,7 +8,7 @@ return {
   opts = {
     -- Specify where to create worktrees relative to git common dir
     -- The common dir is the .git dir in a normal repo or the root dir of a bare repo
-    base_path = '..', -- Parent directory of common dir
+    base_path = './worktrees', -- Parent directory of common dir
 
     -- Template for worktree folder names (string or function(branch) -> path)
     -- This is only used if you don't specify the folder name when creating the worktree
